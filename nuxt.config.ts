9e053beac751ebc3e0a96072fd666e2c742d5c2c
Nirjan Khadka <webdev.nk@gmail.com>
2025-12-14 07:04:03 +0545

    chore: setup new project with nuxt 4

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
